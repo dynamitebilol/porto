73fb1d6716d3d9c7bd1b5d5dcc9f3f47ffed6e25
dynamitebilol <biloltolqunov2505@gmail.com>
2022-03-02 21:30:56 +0500

    Initial commit

---
+++ bilol/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from .models import About, Category, Profile, Portfolio, Skills, Home
+
+admin.site.register(Home)
+
+class ProfileInline(admin.TabularInline):
+    model = Profile
+    extra = 1
+
+@admin.register(About)
+class AboutAdmin(admin.ModelAdmin):
+    inlines = [
+        ProfileInline,
+    ]
+
+class SkillsInline(admin.TabularInline):
+    model = Skills
+    extra = 2
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [
+        SkillsInline,
+    ]
+
+admin.site.register(Portfolio)
